79a0942fdef8485f8ca45b24a79598342b713277
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-03-01 23:04:09 +0100

    feat: create different config for dev/prod environments

---
+++ config/config.js
@@ -0,0 +1,17 @@
+const development = {
+    backendApiUrl: 'http://127.0.0.1:7070',
+    mediaServerUrl: 'http://127.0.0.1:6060',
+};
+
+const production = {
+    backendApiUrl: 'http://api.pisentry.app',
+    mediaServerUrl: 'http://mediaserver.pisentry.app',
+};
+
+// const config = process.env.NODE_ENV === 'production' ? production : development;
+
+const mode = 'development';
+
+const config = mode === 'production' ? production : development;
+
+export default config;
