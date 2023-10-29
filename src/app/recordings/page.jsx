cd99f20d127638fee076ef251303a3c61a448566
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-10-29 22:45:27 +0100

    feat: display camera name in navbar

--- src/app/recordings/page.jsx
+++ src/app/recordings/page.jsx
@@ -1,6 +1,7 @@
 import RecordingsList from '@/components/RecordingsList/RecordingsList.jsx';
 import Link from 'next/link';
 import Players from '@/components/Players/Players.jsx';
+import CameraName from '@/components/CameraName/CameraName.jsx';
 
 export const metadata = {
     title: 'Recordings',
@@ -37,8 +38,9 @@ export default async function RecordingsPage() {
     return (
         <div className="flex flex-col h-full">
             <section>
-                <nav className="my-2 ml-2">
+                <nav className="my-2 flex justify-between items-center">
                     <Link href="/" className="bg-white hover:bg-gray-100 text-gray-800 py-1 px-4 border border-gray-400 rounded shadow">Home</Link>
+                    <CameraName />
                     <Link href="/recordings" className="bg-white hover:bg-gray-100 text-gray-800 py-1 px-4 border border-gray-400 rounded shadow ml-3">Recordings</Link>
                 </nav>
             </section>
