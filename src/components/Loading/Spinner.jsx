6e9440e578fcad99dc26b26a24f0200f9a3fd688
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-11-02 21:07:15 +0100

    style: center the loading spinner on X and Y axis wherever it's used

--- src/components/Loading/Spinner.jsx
+++ src/components/Loading/Spinner.jsx
@@ -1,14 +1,16 @@
 export default function LoadingSpinner({ className }) {
     return (
-        <div
-            className={`inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-gray-500 motion-reduce:animate-[spin_1.5s_linear_infinite] ${className}`}
-            role="status"
-        >
-            <span
-                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+        <div className="h-full w-full flex justify-center items-center">
+            <div
+                className={`inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-gray-500 motion-reduce:animate-[spin_1.5s_linear_infinite] ${className}`}
+                role="status"
             >
-                Loading...
-            </span>
+                <span
+                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
+                >
+                    Loading...
+                </span>
+            </div>
         </div>
     )
 }
