6e98aa1e42c49400dbcf7b94f1bdedfbb2724148
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-11-20 17:52:59 +0100

    feat: remove start/stop streaming buttons not needed anymore

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,30 +5,11 @@ import Link from 'next/link';
 import FetchRequest from '@/helpers/FetchRequest.js';
 import urls from '@/urls.js';
 
-const { streamingApiEndpoint, notificationsApiEndpoint } = urls;
+const { notificationsApiEndpoint } = urls;
 
 export default function HomePage() {
 
     const [pushSubscription, setPushSubscription] = useState(null);
-
-    /**
-     * Streaming action buttons
-     */
-    const onClickStartStreamingButton = () =>
-        new FetchRequest(`${streamingApiEndpoint}/1/start`)
-            .options({
-                method: 'POST',
-                headers: { Authorization: 'mytoken' }
-            })
-            .make();
-
-    const onClickStopStreamingButton = () =>
-        new FetchRequest(`${streamingApiEndpoint}/1/stop`)
-            .options({
-                method: 'POST',
-                headers: { Authorization: 'mytoken' }
-            })
-            .make();
 
     /**
      * Push notifications
@@ -74,21 +55,6 @@ export default function HomePage() {
             <main>
                 <div className="flex flex-col items-center pt-24">
                     <h1 className="text-2xl">Hello on PiSentry !</h1>
-
-                    <div className="mt-3">
-                        <button
-                            onClick={onClickStartStreamingButton}
-                            className="bg-white hover:bg-gray-100 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow"
-                        >
-                            Start streaming
-                        </button>
-                        <button
-                            onClick={onClickStopStreamingButton}
-                            className="bg-white hover:bg-gray-100 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow ml-3"
-                        >
-                            Stop streaming
-                        </button>
-                    </div>
 
                     <div className="mt-3">
                         <button
