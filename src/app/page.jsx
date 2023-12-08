74abcf8101f79c9c7efebdbd35924761255843df
d-roduit <37080343+d-roduit@users.noreply.github.com>
2023-12-08 23:13:25 +0100

    feat: use recordings page as home/main page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,79 +1,29 @@
-'use client';
-
-import { useState } from 'react';
+import RecordingsList from '@/components/RecordingsList/RecordingsList.jsx';
 import Link from 'next/link';
-import FetchRequest from '@/helpers/FetchRequest.js';
-import urls from '@/urls.js';
+import Players from '@/components/Players/Players.jsx';
+import CameraName from '@/components/CameraName/CameraName.jsx';
+import { FaGear, FaVideo } from 'react-icons/fa6';
 
-const { notificationsApiEndpoint } = urls;
-
-export default function HomePage() {
-
-    const [pushSubscription, setPushSubscription] = useState(null);
-
-    /**
-     * Push notifications
-     */
-    const onClickSubscribePushNotificationsButton = async () => {
-        if (!('serviceWorker' in navigator && 'PushManager' in window)) {
-            alert('Service Worker, Push API, or both are not supported');
-            return;
-        }
-
-        try {
-            const serviceWorkerRegistration = await navigator.serviceWorker.ready;
-            const pushSubscription = await serviceWorkerRegistration.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: 'BDMJlhUeJ0cPUbzzGjZpKBhuPV7XoxaCW2RAFY7gIBrg65JIUCE3Ryxutn5NX1FdA5e1w28y45WTi38aqJZ4FSQ' // encoded in Base64
-            });
-
-            setPushSubscription(pushSubscription);
-
-            await new FetchRequest(`${notificationsApiEndpoint}/subscribe`)
-                .options({
-                    method: 'POST',
-                    headers: {
-                        Authorization: 'mytoken',
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ subscription: pushSubscription }),
-                })
-                .make();
-        } catch (err) {
-            console.log('Exception caught while subscribing to push service:', err);
-        }
-    };
-
+export default async function RecordingsPage() {
     return (
-        <>
-            <header>
-                <nav className="my-2 ml-2">
-                    <Link href="/" className="bg-white hover:bg-gray-100 text-gray-800 py-1 px-4 border border-gray-400 rounded shadow">Home</Link>
-                    <Link href="/recordings" className="bg-white hover:bg-gray-100 text-gray-800 py-1 px-4 border border-gray-400 rounded shadow ml-3">Recordings</Link>
+        <div className="flex flex-col h-full">
+            <section>
+                <nav className="flex justify-between items-center mx-5 h-12">
+                    <Link href="/" className="text-xl md:text-2xl text-gray-900">
+                        <FaGear />
+                    </Link>
+                    <CameraName />
+                    <Link href="/camera-settings" className="text-xl md:text-2xl text-gray-900">
+                        <FaVideo />
+                    </Link>
                 </nav>
-            </header>
-            <main>
-                <div className="flex flex-col items-center pt-24">
-                    <h1 className="text-2xl">Hello on PiSentry !</h1>
-
-                    <div className="mt-3">
-                        <button
-                            onClick={onClickSubscribePushNotificationsButton}
-                            className="bg-white hover:bg-gray-100 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow"
-                        >
-                            Subscribe to push notification
-                        </button>
-                    </div>
-
-                    {pushSubscription !== null && (
-                        <p className="py-2 px-4 border border-gray-400">
-                            <b>Subscription:</b>
-                            <br/>
-                            {JSON.stringify(pushSubscription)}
-                        </p>
-                    )}
-                </div>
-            </main>
-        </>
-    )
+            </section>
+            <section>
+                <Players />
+            </section>
+            <section className="overflow-y-scroll">
+                <RecordingsList />
+            </section>
+        </div>
+    );
 }
