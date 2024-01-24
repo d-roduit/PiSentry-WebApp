0ce099a065463f872863bc9c24b64787b6e3daf9
d-roduit <37080343+d-roduit@users.noreply.github.com>
2024-01-24 11:47:00 +0100

    feat: use `tailwind-merge` to be able to overwrite default styles

--- src/components/Loading/Spinner.jsx
+++ src/components/Loading/Spinner.jsx
@@ -1,16 +1,19 @@
+import { twMerge } from 'tailwind-merge';
+
 export default function LoadingSpinner({ className }) {
     return (
-        <div className="h-full w-full flex justify-center items-center">
-            <div
-                className={`inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-gray-500 motion-reduce:animate-[spin_1.5s_linear_infinite] ${className}`}
-                role="status"
+        <div
+            className={twMerge(
+                'h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] text-gray-500 motion-reduce:animate-[spin_1.5s_linear_infinite]',
+                className
+            )}
+            role="status"
+        >
+            <span
+                className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
             >
-                <span
-                    className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-                >
-                    Loading...
-                </span>
-            </div>
+                Loading...
+            </span>
         </div>
     )
 }
